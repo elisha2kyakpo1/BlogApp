db96a08fd24a90348a7b89ca0bac6619812ee3d6
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-04 19:48:40 +0200

    add controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,22 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  def new
+  def new; end
+
+  def show; end
+
+  def update
+    @article = Book.find(params[:id])
+    if @article.update(article_params)
+      redirect_to(@article)
+    else
+      render 'edit'
+    end
   end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 
-  def edit
-  end
+  def edit; end
 end
