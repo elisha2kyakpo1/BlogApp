d48cfd91a9aac4762dd3f8bcb47e6274698db424
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-04 15:34:40 +0200

    add MyBlog controller

---
+++ app/controllers/my_blog_controller.rb
@@ -0,0 +1,2 @@
+class MyBlogController < ApplicationController
+end
---
+++ test/controllers/my_blog_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MyBlogControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
